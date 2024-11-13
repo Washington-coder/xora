1a0f7bf2755f348740d2f4633da5eebe3857d5f3
Washington <was.antonio.xy@gmail.com>
2024-11-13 13:49:07 -0400

    feat: Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from './sections/Header'
 
 const App = () => {
   return (
     <main className="overflow-hidden">
-      
+      <Header />
     </main>
   )
 }
